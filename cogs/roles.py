1afbbb1c1a5fe265c19d09ae62c8cbf7c194ea10
nicholascaro <70615231+nicholascaro@users.noreply.github.com>
2022-01-18 13:45:26 -0800

    Completed Slide 3 To Do

--- cogs/roles.py
+++ cogs/roles.py
@@ -5,10 +5,10 @@ from discord.ext.commands.converter import TextChannelConverter
 from dotenv import load_dotenv
 load_dotenv()
 
+
 class Roles(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
-
 
     @Cog.listener()
     async def on_member_join(self, member):
@@ -20,35 +20,37 @@ class Roles(Cog):
     async def on_message(self, message):
         if message.content == "Hi":
             await message.channel.send("Hello")
-    
+
     @Cog.listener()
     async def on_member_join(self, member):
-        role = discord.utils.get(member.guild.roles, id=int(os.environ.get('NEWCOMER'))) #NEWCOMER is the id of the NewComer role in my server
+        # NEWCOMER is the id of the NewComer role in my server
+        role = discord.utils.get(
+            member.guild.roles, id=int(os.environ.get('NEWCOMER')))
         await member.add_roles(role)
 
-        #What happened to the previous code??
+        # What happened to the previous code??
         # A: We decided to use 'Cogs'. Essentially, cogs allow you to organize your code with classes.
         #    You will notice we now have a 'Roles.py' file that handles all the events in our 'Roles' class
         #    And, we also have a 'Schedules.py' file that handles all the events/commands in our 'Schedules' class
         #    Although you might see slight changes, don't worry. The same logic we've learned still applies!
 
-        # TO DO: 
+        # TO DO:
         # - Send a message in your #Get-Roles text channel
-        # - Using 'on_raw_reaction_add' make another listener event that gives a member role depending on their 
+        # - Using 'on_raw_reaction_add' make another listener event that gives a member role depending on their
         #   reaction to the message you sent in the #Get-Roles channel
-            # (For example, if the user reacts to the message with the :acmDev: emoji, then that user
-            # will be added to the 'Dev Member' Role.)
+        # (For example, if the user reacts to the message with the :acmDev: emoji, then that user
+        # will be added to the 'Dev Member' Role.)
         # - There are three roles they can choose to join, 'Dev Member', 'Algo Member', and 'Create Member'.
 
-        #Steps to do this:
+        # Steps to do this:
         # 1) Store each role into a variable using 'discord.utils.get'
         # 2)Store the id of the designated #Get-Roles message into a variable
         # 3)Check if the payload message id matches the designated #Get-Roles message id
         # 4)If they do match, use if statements to handle each of the three possible reactions
         #   (Ex.If the emoji id equal's that of the :acmDev: emoji, give them the 'Dev Member' Role)
-        # 5)Use the 'add_roles' function to add the member to a role 
+        # 5)Use the 'add_roles' function to add the member to a role
 
-        #TIPS/HINTS
+        # TIPS/HINTS
         # - In order to get the id of a emoji, type/enter into a text channel "\[emoji_name_goes_here"
         # - For example, if the emoji's name was 'happy' you would type/enter into a text channel
         #   '\happy'
@@ -58,8 +60,31 @@ class Roles(Cog):
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
         # YOUR CODE GOES HERE
+<<<<<<< HEAD
+=======
+        pass
     
+>>>>>>> e69a1eb375a3a12730628cb27a64f58a88211e85
 
+        devM = discord.utils.get(
+            payload.member.guild.roles, id=916198780754346017)
+        algoM = discord.utils.get(
+            payload.member.guild.roles, id=916198780754346018)
+        createM = discord.utils.get(
+            payload.member.guild.roles, id=916198780754346019)
+
+        emojiID = {'dev': 930209176431505509,
+                   'algo': 930209120907296799, 'create': 930209148543578213}
+
+        role_message_id = 932456799607599144
+
+        if payload.message_id == role_message_id:
+            if payload.emoji_id == emojiID['dev']:
+                await payload.member.add_roles(devM)
+            elif payload.emoji_id == emojiID['algo']:
+                await payload.member.add_roles(algoM)
+            elif payload.emoji_id == emojiID['create']:
+                await payload.member.add_roles(createM)
 
 
 def setup(bot: Bot):
--- cogs/schedules.py
+++ cogs/schedules.py
@@ -4,36 +4,12 @@ from discord_slash import cog_ext, SlashContext
 from os import path
 import json
 from helpers import getGuilds
+import discord
 
 guild_ids = getGuilds()
 
 classes_path = path.relpath("data/classes.json")
 students_path = path.relpath("data/students.json")
-
-# Example of reading from a json file
-
-with open(classes_path, 'r') as openfile:
-    json_object = json.load(openfile)
-    print(json_object)
-
-
-# Example of writing to a json file 
-
-class_test_dict = {
-        "CPSC 131-05": ["Aaron"],
-        "MATH 338-01": ["Bob", "Kevin"]
-}
-
-class_json = json.dumps(class_test_dict)
-
-with open(classes_path, "w") as outfile:
-    outfile.write(class_json)
-
-with open(classes_path) as f:
-  classes = json.load(f)
-
-with open(students_path) as f:
-  students = json.load(f)
 
 class Schedules(Cog):
     def __init__(self, bot: Bot):
@@ -41,7 +17,16 @@ class Schedules(Cog):
 
     @cog_ext.cog_subcommand(base="schedule", name="view", description="View your schedule", guild_ids=guild_ids)
     async def _schedule_view(self, ctx: SlashContext):
-        await ctx.send(content="schedule")
+        embed=discord.Embed(title="Hello " + ctx.author.display_name + "!", 
+    description="Here are your classes:", 
+    color=0xFF5733)
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+        classes = students[ctx.author.display_name]
+        for class_name in classes:
+            idx = class_name.index("-")
+            class_session = class_name[idx+1:]
+            embed.add_field(name=class_name[:idx], value="Session: " + class_session, inline=False)
+        await ctx.send(embed=embed)
 
     @cog_ext.cog_subcommand(base="schedule", name="add", description="Add a class to your schedule", guild_ids=guild_ids)
     async def _schedule_add(self, ctx: SlashContext, text: str):
@@ -51,33 +36,5 @@ class Schedules(Cog):
     async def _schedule_remove(self, ctx: SlashContext, text: str):
         await ctx.send(content="schedule remove")
 
-    # two json files, classes.json, students.json
-
-    # /schedule 
-        # open the students.json file
-        # return the students classes
-        # display button asking to share to the channel
-
-    # /schedule add
-        # open both json files
-        # validate the input
-        # add the class to the person
-        # add the person to the class
-        # give them the role/permissions
-        
-    # /schedule remove
-        # open both json files
-        # check if the student is in that class
-        # remove the class from the student
-        # remove the student from the class
-        # update their roles/permissions
-    
-    """
-    Create your own command that allows a user to run /schedule, and it will return a link to the google form ("https://docs.google.com/forms/d/1sz-YZ3QkpTkVMFYlX8Mt0k1jhJrSY1BPzXMMUVdCEXs/edit")
-
-    Create a description for the command as well.
-    """
-
 def setup(bot: Bot):
     bot.add_cog(Schedules(bot))
-
